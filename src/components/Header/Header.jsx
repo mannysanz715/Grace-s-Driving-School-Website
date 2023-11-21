8c3e25b628c5a9e1623f85997e63e80d0eb3ca31
Manny Sanchez <mannysanz715@gmail.com>
2023-11-21 11:45:28 -0800

    Smaller width for header text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,8 +11,8 @@ function Header(){
 
   return(
     <div  className="flex flex-col header max-sm:justify-evenly max-sm:gap- justify-between py-16 items-center w-screen h-screen">
-      <div className='flex flex-col items-center'>
-      <img data-aos="zoom-in"  src="/assets/logo.png" className="w-5/12 max-sm:w-8/12" alt="school logo" />
+      <div className='flex flex-col items-center w-10/12'>
+      <img data-aos="zoom-in"  src="/assets/logo.png" className="w-3/12 max-sm:w-8/12" alt="school logo" />
         <p className='text-white text-4xl font-extrabold text-center '>Your #1 Choice For Driving Lessons</p>
         <p className='text-white text-4xl font-bold text-center '>Learn To Drive With Patient Instructors</p>
 

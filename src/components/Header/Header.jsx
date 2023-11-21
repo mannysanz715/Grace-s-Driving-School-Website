64eb6a476c28b29a28952c74b2b7d2eeb4734b23
Manny Sanchez <mannysanz715@gmail.com>
2023-11-21 11:46:27 -0800

    Change width again

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header(){
 
   return(
     <div  className="flex flex-col header max-sm:justify-evenly max-sm:gap- justify-between py-16 items-center w-screen h-screen">
-      <div className='flex flex-col items-center w-10/12'>
+      <div className='flex flex-col items-center w-11/12'>
       <img data-aos="zoom-in"  src="/assets/logo.png" className="w-3/12 max-sm:w-8/12" alt="school logo" />
         <p className='text-white text-4xl font-extrabold text-center '>Your #1 Choice For Driving Lessons</p>
         <p className='text-white text-4xl font-bold text-center '>Learn To Drive With Patient Instructors</p>

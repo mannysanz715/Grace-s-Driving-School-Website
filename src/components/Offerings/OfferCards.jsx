51529a120526e2571807eead1a3764cdd14182ea
Manny Sanchez <mannysanz715@gmail.com>
2023-11-21 11:43:09 -0800

    Rounded Servives Cards

--- src/components/Offerings/OfferCards.jsx
+++ src/components/Offerings/OfferCards.jsx
@@ -15,7 +15,7 @@ function OfferCards({title, buttonText, buttonText2, buttonLink2, image, buttonL
 
 
   return(
-    <div data-aos='flip-left' className="bg-sky-400 w-3/6 h-[18rem] max-sm:flex-col-reverse gap-5 max-sm:h-fit max-sm:text-center max-sm:w-72 flex justify-evenly items-center p-5 opacity-90">
+    <div data-aos='flip-left' className="bg-sky-400 w-3/6 h-[18rem] max-sm:flex-col-reverse gap-5 max-sm:h-fit max-sm:text-center max-sm:w-72 flex justify-evenly items-center p-5 rounded opacity-90">
      <div className="flex flex-col items-center justify-center gap-8 w-[18rem]">
         <h1 className="text-white font-bold text-4xl text-center max-sm:pb-2 max-sm:text-xl">{title}</h1>
         {type == 'link' ? <div className="flex gap-10">

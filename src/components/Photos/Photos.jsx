a25c768bb28d164b3947375fa211cc2b10dcfc74
Manny Sanchez <mannysanz715@gmail.com>
2023-12-28 19:46:03 -0800

    lazy load images

--- src/components/Photos/Photos.jsx
+++ src/components/Photos/Photos.jsx
@@ -6,20 +6,20 @@ function Photos(){
           <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
 
               <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                 <img src="/assets/onSite.jpg" className="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                 <img src="/assets/onSite.jpg" loading="lazy" className="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
 
               </div>
 
               <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="/assets/image2.png" className="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                  <img src="/assets/image2.png" loading="lazy" className="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
               </div>
 
               <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="/assets/image3.png" className="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                  <img src="/assets/image3.png" loading="lazy" className="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
               </div>
 
               <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="/assets/image1.png" className="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                  <img src="/assets/image1.png" loading="lazy" className="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
               </div>
 
           </div>

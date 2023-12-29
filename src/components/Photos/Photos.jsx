cfc76df8dcbe3d3e38cae7648c2354787791127f
Manny Sanchez <mannysanz715@gmail.com>
2023-12-28 19:57:38 -0800

    update photos

--- src/components/Photos/Photos.jsx
+++ src/components/Photos/Photos.jsx
@@ -19,7 +19,7 @@ function Photos(){
               </div>
 
               <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="/assets/image1.png" loading="lazy" className="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                  <img src="/assets/image1.jpg" loading="lazy" className="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
               </div>
 
           </div>

2436b13cbbbee38e59ec9886c482ffa5f38e1c73
Manny Sanchez <mannysanz715@gmail.com>
2024-01-15 15:26:18 -0800

    temp solution to images maybe

--- src/components/Photos/Photos.jsx
+++ src/components/Photos/Photos.jsx
@@ -5,7 +5,7 @@ function Photos(){
 
           <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
 
-              <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <div className=" duration-700 ease-in-out" data-carousel-item>
                  <img src="/assets/onSite.jpg"  className="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
 
               </div>

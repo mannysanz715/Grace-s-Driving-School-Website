d15d7a100c3030fb5af3b7acd5d0edd9de3f4904
Manny Sanchez <mannysanz715@gmail.com>
2024-09-02 17:17:01 -0700

    Update payments page, added cancellation fee, extra hour, DMV Test

--- src/pages/Payments/Payments.jsx
+++ src/pages/Payments/Payments.jsx
@@ -18,6 +18,18 @@ function Payments(){
           <h1 className="payment-title">5 Lessons Package Payment:</h1>
           <BuyButtonComponent buyButtonId={'buy_btn_1NyJ1oGBwh195q0L4bi4jeDa'} pubKey={'pk_live_51NxxpIGBwh195q0LHvF4Jqrjf8mFRprri72Xt63KKQ4x3bPOS2cfGeAlh4Gi56JvgXgAidoRN5v971de4slfivHD0075Hh4m1B'}/>
         </div>
+        <div className="payment-container">
+          <h1 className="payment-title">Behind the Wheel Drive Test:</h1>
+          <BuyButtonComponent buyButtonId={'buy_btn_1PukETGBwh195q0LXbk6szC6'} pubKey={'pk_live_51NxxpIGBwh195q0LHvF4Jqrjf8mFRprri72Xt63KKQ4x3bPOS2cfGeAlh4Gi56JvgXgAidoRN5v971de4slfivHD0075Hh4m1B'}/>
+        </div>
+        <div className="payment-container">
+          <h1 className="payment-title">Additional Hour:</h1>
+          <BuyButtonComponent buyButtonId={'buy_btn_1PukOnGBwh195q0Lc3U2OuOc'} pubKey={'pk_live_51NxxpIGBwh195q0LHvF4Jqrjf8mFRprri72Xt63KKQ4x3bPOS2cfGeAlh4Gi56JvgXgAidoRN5v971de4slfivHD0075Hh4m1B'}/>
+        </div>
+        <div className="payment-container">
+          <h1 className="payment-title">Cancellation Fee:</h1>
+          <BuyButtonComponent buyButtonId={'buy_btn_1PukKZGBwh195q0Lq7kdhUPP'} pubKey={'pk_live_51NxxpIGBwh195q0LHvF4Jqrjf8mFRprri72Xt63KKQ4x3bPOS2cfGeAlh4Gi56JvgXgAidoRN5v971de4slfivHD0075Hh4m1B'}/>
+        </div>
 
       </div>
   )

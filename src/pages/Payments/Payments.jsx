5570b9aeddc47192a4d7974789e2003ac2f3803d
Manny Sanchez <mannysanz715@gmail.com>
2025-05-15 18:11:58 -0700

    Create non local drive test buy button on payments page

--- src/pages/Payments/Payments.jsx
+++ src/pages/Payments/Payments.jsx
@@ -26,6 +26,11 @@ function Payments(){
           <BuyButtonComponent buyButtonId={'buy_btn_1PukETGBwh195q0LXbk6szC6'} pubKey={'pk_live_51NxxpIGBwh195q0LHvF4Jqrjf8mFRprri72Xt63KKQ4x3bPOS2cfGeAlh4Gi56JvgXgAidoRN5v971de4slfivHD0075Hh4m1B'}/>
         </div>
         <div className="payment-container">
+          <h1 className="payment-title">Behind the Wheel Drive Test:</h1>
+          <p className="hours">(Not Local)</p>
+          <BuyButtonComponent buyButtonId={'buy_btn_1RPClFGBwh195q0LKwMdfC34'} pubKey={'pk_live_51NxxpIGBwh195q0LHvF4Jqrjf8mFRprri72Xt63KKQ4x3bPOS2cfGeAlh4Gi56JvgXgAidoRN5v971de4slfivHD0075Hh4m1B'}/>
+        </div>
+        <div className="payment-container">
           <h1 className="payment-title">Additional Hour:</h1>
           <BuyButtonComponent buyButtonId={'buy_btn_1PukOnGBwh195q0Lc3U2OuOc'} pubKey={'pk_live_51NxxpIGBwh195q0LHvF4Jqrjf8mFRprri72Xt63KKQ4x3bPOS2cfGeAlh4Gi56JvgXgAidoRN5v971de4slfivHD0075Hh4m1B'}/>
         </div>
